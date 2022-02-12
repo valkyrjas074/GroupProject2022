783305e3202afcd12da301b96996b016d23b62ad
lplong <longle.phc@gmail.com>
2022-02-12 11:11:24 +0700

    update run crawler by commmand with url

--- web_scrapping/web_scrapping/spiders/photos.py
+++ web_scrapping/web_scrapping/spiders/photos.py
@@ -1,26 +1,21 @@
 import scrapy
-from scrapy.crawler import CrawlerProcess
 
 class PhotosSpider(scrapy.Spider):
     name = 'photos'
+    
+    def __init__(self, *args, **kwargs): 
+      super(PhotosSpider, self).__init__(*args, **kwargs) 
 
-    def start_requests(self):
-        yield scrapy.Request('https://www.nytimes.com/')
+      self.start_urls = [kwargs.get('start_url')]
+
+
 
     def parse(self, response):
         raw_image_urls = response.css('img ::attr(src)').getall()
-        clean_image_urls=[]
+        clean_image_urls = []
         for img_url in raw_image_urls:
             clean_image_urls.append(response.urljoin(img_url))
         
         yield {
             'image_urls': clean_image_urls
         }
-        
-process = CrawlerProcess(settings = {
-    'FEED_URI': 'photos_url.json',
-    'FEED_FORMAT': 'json'
-})
-
-process.crawl(PhotosSpider)
-process.start()
